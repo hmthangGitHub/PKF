3a7dcd5bab0f5f9dc113d915fd58dd4ebb1011cf
Nolan <nolan.huang@a5labs.co>
2024-01-10 11:21:10 +0800

    Add pkw client files

--- scripts/poker-client/poker-client.ts
+++ scripts/poker-client/poker-client.ts
@@ -8,6 +8,7 @@ import type {
     User
 } from './poker-client-types';
 import type { ISocket } from './poker-socket';
+import { PKWClient } from './pkw/pkw-client';
 
 export interface IPokerClient {
     login(username: string, password: string, options?: RequestOtpions): Promise<ISession>;
@@ -21,6 +22,9 @@ export interface IPokerClient {
 
 export class PokerClient {
     static create(serverType: PlatformType, host: string, options: IClientOptions): IPokerClient {
+        if (serverType === 'pkw') {
+            return new PKWClient(host, options);
+        }
         if (serverType === 'wpk') {
             return new WPKClient(host, options);
         } else {
