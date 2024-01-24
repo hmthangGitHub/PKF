31cc9ea638dc13fb286c0a7ffef334eb017168dd
Ray <pin-jui.luoh@a5labs.co>
2024-01-24 17:23:49 +0800

    move resources and scripts of Avatar and CommonDialog to common resource asset bundle

--- scripts/components/components-index.ts
+++ scripts/components/components-index.ts
@@ -1,6 +1,6 @@
 export * from './ScrollViewReuse';
 export * from './TagCom';
-export * from './AvatarControl';
+// export * from './AvatarControl';
 export * from './advanced-auto-bet/advanced-auto-bet-index';
 export * from './HeadPointsAni';
-export * from './CommonDialogControl';
+// export * from './CommonDialogControl';
