6ebe2d2792e875f6a14866f3e18fe69c536af9ab
Nolan <nolan.huang@a5labs.co>
2024-02-05 17:03:57 +0800

    Wrap md5 to util

---
+++ scripts/core/utils/crytpo.ts
@@ -0,0 +1,7 @@
+import * as md5 from 'md5';
+
+export class Crypto {
+    static md5(message: string | number[] | Buffer, options?: Pick<md5.Options, 'asString' | 'encoding'>): string {
+        return md5(message, options);
+    }
+}
