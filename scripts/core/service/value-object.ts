48bf42832b4559c26a1746467d25d94ea55d1a91
Nolan <nolan.huang@a5labs.co>
2024-02-05 15:59:59 +0800

    Refactor ValueObject

--- scripts/core/service/value-object.ts
+++ scripts/core/service/value-object.ts
@@ -1,39 +1,51 @@
-export interface IDomainValueObject {
-    from(data?: any): void;
+export interface IValueObject {
+    /** clone form proto object */
+    fromProto(data?: any): void;
 }
 
-export interface DomainValueClass<T> {
+export interface ValueObjectClass<T> {
     // eslint-disable-next-line @typescript-eslint/prefer-function-type
     new (): T;
-
-    create(data?: any): T;
 }
 
-export class DomainValueObject {
-    static createFromProto<T extends IDomainValueObject, DataType>(
-        domainClass: DomainValueClass<T>,
-        data?: DataType
-    ): T {
+export class ValueObject {
+    /** create value other form proto object */
+    static fromProto<T extends IValueObject, DataType>(domainClass: ValueObjectClass<T>, data?: DataType): T {
         // eslint-disable-next-line new-cap
         const instance = new domainClass();
         if (data) {
-            instance.from(data);
+            instance.fromProto(data);
         }
         return instance;
     }
 }
 
-export class DomainValueObjectArray {
-    static cloneFromProto<T extends IDomainValueObject, ProtoType>(
-        domainValueClass: DomainValueClass<T>,
+export class ValueObjectArray {
+    /** create value object array form proto object array */
+    static fromProto<T extends IValueObject, ProtoType>(
+        valueObjectClass: ValueObjectClass<T>,
+        protoArray?: ProtoType[]
+    ): T[] {
+        const valueObjectArray = new Array<T>();
+        // clone proto array if exist
+        protoArray?.forEach((item: ProtoType) => {
+            valueObjectArray.push(ValueObject.fromProto(valueObjectClass, item));
+        });
+
+        return valueObjectArray;
+    }
+
+    /** clone specific value object array form proto object array */
+    static cloneFromProto<T extends IValueObject, ProtoType>(
+        valueObjectClass: ValueObjectClass<T>,
         valueObjectArray: T[],
         protoArray?: ProtoType[]
     ) {
         // clear array
         valueObjectArray.length = 0;
         // clone proto array if exist
-        protoArray?.forEach((item) => {
-            valueObjectArray.push(domainValueClass.create(item));
+        protoArray?.forEach((item: ProtoType) => {
+            valueObjectArray.push(ValueObject.fromProto(valueObjectClass, item));
         });
     }
 }
