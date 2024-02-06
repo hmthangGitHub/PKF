d1fc638a3a3487e970071da2152d36661880ec9e
Nolan <nolan.huang@a5labs.co>
2024-02-06 19:56:09 +0800

    Fix pf invalid export

--- scripts/pf.ts
+++ scripts/pf.ts
@@ -1,4 +1,3 @@
 export * from './core/core-index';
 export * as services from './services/services-index';
-export * as components from './components/components-index';
 export * from './poker-client/poker-client-index';
