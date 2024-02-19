55ea9d6e30b384ec332794c6ca0b114d5cd7f374
Nolan <nolan.huang@a5labs.co>
2024-02-19 14:25:08 +0800

    Refactor GameService

--- scripts/services/game/game-service.ts
+++ scripts/services/game/game-service.ts
@@ -1,5 +1,6 @@
-import type { IService } from '../../core/core-index';
 import { TypeSafeEventEmitter } from '../../core/core-index';
+import type { IService } from '../../core/core-index';
+import type { IJoinRoomResponse } from '../../poker-client/game-session';
 
 export interface IGameService extends IService {
     serverId: number;
@@ -14,4 +15,10 @@ export abstract class GameService<EventType> extends TypeSafeEventEmitter<EventT
     }
 
     abstract get serverId(): number;
+
+    abstract login(): Promise<void>;
+
+    abstract joinRoom(roomId: number): Promise<IJoinRoomResponse>;
+
+    abstract leaveRoom(): Promise<void>;
 }
