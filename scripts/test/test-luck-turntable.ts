6013ead9d2231107096862b1c7d8965690309cdd
Ray <pin-jui.luoh@a5labs.co>
2024-03-01 14:11:06 +0800

    add event handling to app for red envelope tooltip
    add luck turntable local test related resources

---
+++ scripts/test/test-luck-turntable.ts
@@ -0,0 +1,48 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+import * as pf from '../../../poker-framework/scripts/pf';
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class TestLuckTurntable extends cc.Component {
+    private _luckTurntableService: pf.services.LuckTurntableService = null;
+
+    onLoad() {
+        this._luckTurntableService = pf.serviceManager.get(pf.services.LuckTurntableService);
+    }
+
+    start() {}
+
+    testStartTime() {
+        this._luckTurntableService.testStartTime();
+    }
+
+    testEndTime() {
+        this._luckTurntableService.testEndTime();
+    }
+
+    testReady() {
+        this._luckTurntableService.testReady();
+    }
+
+    testCountdown() {
+        this._luckTurntableService.testCountdown();
+    }
+
+    testOver() {
+        this._luckTurntableService.testOver();
+    }
+
+    testDraw() {
+        this._luckTurntableService.testDraw();
+    }
+
+    testResult() {
+        this._luckTurntableService.testResult();
+    }
+}
