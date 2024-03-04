712a31b7210e4d2cf441e571039f3a2ea0400151
Ray <pin-jui.luoh@a5labs.co>
2024-03-04 14:41:25 +0800

    change the way to get user data

--- scripts/services/auth-service.ts
+++ scripts/services/auth-service.ts
@@ -30,10 +30,6 @@ export class AuthService extends EmittableService<AuthEvents> {
         this._client = client;
     }
 
-    get userData(): IUser {
-        return this._currentUser;
-    }
-
     async login(username: string, password: string, options?: RequestOtpions): Promise<ISession> {
         const session = await this._client.login(username, password, options);
 
@@ -47,36 +43,36 @@ export class AuthService extends EmittableService<AuthEvents> {
     }
 
     async getUserData(): Promise<IResponseGetUserData> {
-        const response = this._client.getSocket().getUserData(this._currentUser.userId);
+        const response = this._client.getSocket().getUserData(this.currentUser.userId);
         return response;
     }
 
     onUserDataNotify(notify: INoticeGetUserData) {
         cc.log(notify);
-        this._currentUser.nickname = notify.nick_name;
-        this._currentUser.userGold = notify.user_gold;
-        this._currentUser.totalAmount = notify.total_amount;
-        this._currentUser.gameCoin = notify.game_coin;
-        this._currentUser.userPoints = notify.user_points;
-        this._currentUser.ratio = notify.ratio;
-        this._currentUser.avatarURL = notify.avatar;
-        // this._currentUser.userId = notify.user_id;
-        this._currentUser.mobile = notify.mobile;
-        this._currentUser.sex = notify.gender;
-        this._currentUser.userMarks = notify.user_marks;
-        this._currentUser.clubsMax = notify.clubs_max;
-        this._currentUser.currentClubs = notify.current_clubs;
-        this._currentUser.cardType = notify.card_type;
-        this._currentUser.depositGold = notify.deposit_gold;
-        this._currentUser.usdt = notify.usdt;
-        this._currentUser.depositUsdt = notify.usdt;
-        this._currentUser.priorityAreaCode = notify.areaCode;
-        this._currentUser.priorityMobile = notify.mobile2;
-        this._currentUser.sportsTrialCoin = notify.sports_trial_coin.coins;
-        this._currentUser.sportsTrialCoinExpiration = notify.sports_trial_coin.expired_at;
-        this._currentUser.sportsBettingBalance = notify.sports_betting_balance;
-        this._currentUser.systemTime = notify.system_time;
-        this._currentUser.calmDownDeadlineTime = notify.calm_down_deadline_time;
+        this.currentUser.nickname = notify.nick_name;
+        this.currentUser.userGold = notify.user_gold;
+        this.currentUser.totalAmount = notify.total_amount;
+        this.currentUser.gameCoin = notify.game_coin;
+        this.currentUser.userPoints = notify.user_points;
+        this.currentUser.ratio = notify.ratio;
+        this.currentUser.avatarURL = notify.avatar;
+        // this.currentUser.userId = notify.user_id;
+        this.currentUser.mobile = notify.mobile;
+        this.currentUser.sex = notify.gender;
+        this.currentUser.userMarks = notify.user_marks;
+        this.currentUser.clubsMax = notify.clubs_max;
+        this.currentUser.currentClubs = notify.current_clubs;
+        this.currentUser.cardType = notify.card_type;
+        this.currentUser.depositGold = notify.deposit_gold;
+        this.currentUser.usdt = notify.usdt;
+        this.currentUser.depositUsdt = notify.usdt;
+        this.currentUser.priorityAreaCode = notify.areaCode;
+        this.currentUser.priorityMobile = notify.mobile2;
+        this.currentUser.sportsTrialCoin = notify.sports_trial_coin.coins;
+        this.currentUser.sportsTrialCoinExpiration = notify.sports_trial_coin.expired_at;
+        this.currentUser.sportsBettingBalance = notify.sports_betting_balance;
+        this.currentUser.systemTime = notify.system_time;
+        this.currentUser.calmDownDeadlineTime = notify.calm_down_deadline_time;
 
         this.emit('userData');
     }
