335d6bd30e2e8581fb50ec7cd813540b78dfefee
Edward <wu.si-xian@a5labs.co>
2024-03-27 21:15:06 +0800

    fix: fix setPortrait's implement for android

--- scripts/core/native/env/android-native-sdk.ts
+++ scripts/core/native/env/android-native-sdk.ts
@@ -11,10 +11,9 @@ export class AndroidNativeSDK {
     invoke(action: NativeInvokeAction): string {
         if(!action.methodSignature) throw new Error("[pf][AndroidNativeSDK] invoking need to be pass action's methodSignature")
         // NOTE: isSync: true, Android下: 在gl线程调用原生函数
-        // NOTEisSync: false, Android下: 在UI线程调用原生函数，此时返回值没有意义
+        // NOTE: isSync: false, Android下: 在UI线程调用原生函数，此时返回值没有意义
         const jsonParam = this._nativeSDK.getJSONParam(action.obj, action.method, action.respMsgKey, action.param, action.isSync);
-        // return jsb.reflection.callStaticMethod('org/cocos2dx/javascript/NativeEvent', 'call_native', '(Ljava/lang/String;)Ljava/lang/String;', jsonParam);
-        return jsb.reflection.callStaticMethod('org/cocos2dx/javascript/NativeEvent', 'call_native', action.methodSignature, jsonParam);
+        return jsb.reflection.callStaticMethod(action.obj, action.method, action.methodSignature, jsonParam);
     }
 
     // NOTE: Android native callback
