691db7a43460e10557c46e61248db5b4f03a24c2
Pin-Jui Luoh <pin-jui.luoh@a5labs.co>
2024-07-30 10:40:25 +0800

    fix for AT-5375
    some properties are not provided in noticeGetUserData in wpk, ex: sports related properties
    add a function in context for updating user balance in mini game menu

--- scripts/services/auth-service.ts
+++ scripts/services/auth-service.ts
@@ -49,30 +49,30 @@ export class AuthService extends EmittableService<AuthEvents> {
 
     onUserDataNotify(notify: INoticeGetUserData) {
         cc.log(notify);
-        this.currentUser.nickname = notify.nick_name;
-        this.currentUser.userGold = notify.user_gold;
-        this.currentUser.totalAmount = notify.total_amount;
-        this.currentUser.gameCoin = notify.game_coin;
-        this.currentUser.userPoints = notify.user_points;
-        this.currentUser.ratio = notify.ratio;
-        this.currentUser.avatarURL = notify.avatar;
+        this.currentUser.nickname = notify.nick_name ?? '';
+        this.currentUser.userGold = notify.user_gold ?? 0;
+        this.currentUser.totalAmount = notify.total_amount ?? 0;
+        this.currentUser.gameCoin = notify.game_coin ?? 0;
+        this.currentUser.userPoints = notify.user_points ?? 0;
+        this.currentUser.ratio = notify.ratio ?? 0;
+        this.currentUser.avatarURL = notify.avatar ?? '';
         // this.currentUser.userId = notify.user_id;
-        this.currentUser.mobile = notify.mobile;
-        this.currentUser.sex = notify.gender;
-        this.currentUser.userMarks = notify.user_marks;
-        this.currentUser.clubsMax = notify.clubs_max;
-        this.currentUser.currentClubs = notify.current_clubs;
-        this.currentUser.cardType = notify.card_type;
-        this.currentUser.depositGold = notify.deposit_gold;
-        this.currentUser.usdt = notify.usdt;
-        this.currentUser.depositUsdt = notify.usdt;
-        this.currentUser.priorityAreaCode = notify.areaCode;
-        this.currentUser.priorityMobile = notify.mobile2;
-        this.currentUser.sportsTrialCoin = notify.sports_trial_coin.coins;
-        this.currentUser.sportsTrialCoinExpiration = notify.sports_trial_coin.expired_at;
-        this.currentUser.sportsBettingBalance = notify.sports_betting_balance;
-        this.currentUser.systemTime = notify.system_time;
-        this.currentUser.calmDownDeadlineTime = notify.calm_down_deadline_time;
+        this.currentUser.mobile = notify.mobile ?? '';
+        this.currentUser.sex = notify.gender ?? 0;
+        this.currentUser.userMarks = notify.user_marks ?? '';
+        this.currentUser.clubsMax = notify.clubs_max ?? 0;
+        this.currentUser.currentClubs = notify.current_clubs ?? 0;
+        this.currentUser.cardType = notify.card_type ?? 0;
+        this.currentUser.depositGold = notify.deposit_gold ?? 0;
+        this.currentUser.usdt = notify.usdt ?? 0;
+        this.currentUser.depositUsdt = notify.deposit_usdt ?? 0;
+        this.currentUser.priorityAreaCode = notify.areaCode ?? '';
+        this.currentUser.priorityMobile = notify.mobile2 ?? '';
+        this.currentUser.sportsTrialCoin = notify.sports_trial_coin?.coins ?? 0;
+        this.currentUser.sportsTrialCoinExpiration = notify.sports_trial_coin?.expired_at ?? 0;
+        this.currentUser.sportsBettingBalance = notify.sports_betting_balance ?? 0;
+        this.currentUser.systemTime = notify.system_time ?? 0;
+        this.currentUser.calmDownDeadlineTime = notify.calm_down_deadline_time ?? 0;
 
         this.emit('userData');
     }
