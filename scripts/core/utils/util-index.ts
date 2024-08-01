dcd3b18f068bb2843331c8bf883c28095d08d219
Nolan <nolan.huang@a5labs.co>
2024-08-01 15:22:27 +0800

    Modify crypto filename

--- scripts/core/utils/util-index.ts
+++ scripts/core/utils/util-index.ts
@@ -1,5 +1,5 @@
 export * from './util';
 export * from './ui-util';
 export * from './string-util';
-export * from './crytpo';
+export * from './crypto';
 export * from './compare-versions/compare-versions';
