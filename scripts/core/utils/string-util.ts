489c1ebfb51a7808719941de57832120633958f0
qw <>
2024-08-20 15:31:40 +0900

    修改计算时间天数差逻辑

--- scripts/core/utils/string-util.ts
+++ scripts/core/utils/string-util.ts
@@ -379,13 +379,13 @@ export class StringUtil {
      * 获取目标日期距离当前日期的天数差(暂只添加天数差)
      * @param millisecond
      */
-    static getDateDifference(millisecond: number): number {
+    static getDateDifference(millisecond: number, intl?: Intl.LocalesArgument, timeZone?: string): number {
         let value = 0;
-        let tarDate: any = new Date(millisecond);
-        let nowDate: any = new Date();
+        let tarDate: any = intl ? new Date(millisecond).toLocaleString(intl, { timeZone }) : new Date(millisecond);
+        let nowDate: any = intl ? new Date().toLocaleString(intl, { timeZone }) : new Date();
 
-        tarDate = new Date(tarDate.getFullYear(), tarDate.getMonth(), tarDate.getDay());
-        nowDate = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDay());
+        tarDate = intl ? new Date(tarDate) : new Date(tarDate.getFullYear(), tarDate.getMonth(), tarDate.getDay());
+        nowDate = intl ? new Date(nowDate) : new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDay());
 
         value = (tarDate - nowDate) / (1000 * 60 * 60 * 24);
         return value;
