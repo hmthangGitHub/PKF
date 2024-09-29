c40e550afe3d31ab3b1539e1764bf503315f3e7f
qw <>
2024-09-29 15:24:37 +0900

    add date-util file and migrate getDateDifference function

--- scripts/core/utils/string-util.ts
+++ scripts/core/utils/string-util.ts
@@ -376,26 +376,6 @@ export class StringUtil {
     }
 
     /**
-     * 获取目标日期距离当前日期的天数差(暂只添加天数差)
-     * @param millisecond 时间戳（毫秒）
-     * @param intl 国家形式
-     * @param timeZone 时区
-     */
-    static getDateDifference(millisecond: number, intl?: Intl.LocalesArgument, timeZone?: string): number {
-        let value = 0;
-        let tarDate: any = intl
-            ? new Date(new Date(millisecond).toLocaleString(intl, { timeZone }))
-            : new Date(millisecond);
-        let nowDate: any = intl ? new Date(new Date().toLocaleString(intl, { timeZone })) : new Date();
-
-        tarDate = new Date(tarDate.getFullYear(), tarDate.getMonth(), tarDate.getDay());
-        nowDate = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDay());
-
-        value = (tarDate - nowDate) / (1000 * 60 * 60 * 24);
-        return value;
-    }
-
-    /**
      * 格式化倒计时时间
      * @param leftTime      剩余秒数
      * @param type          时间类型
--- scripts/core/utils/util-index.ts
+++ scripts/core/utils/util-index.ts
@@ -1,5 +1,6 @@
 export * from './util';
 export * from './ui-util';
 export * from './string-util';
+export * from './date-util';
 export * from './crypto';
 export * from './compare-versions/compare-versions';
