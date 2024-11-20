ed37fb6064c394c4047db5a8648a1f8af68c92a1
Nolan <nolan.huang@a5labs.co>
2024-11-20 16:07:20 +0800

    Remove unnecessary logs

--- scripts/core/native/native-sdk.ts
+++ scripts/core/native/native-sdk.ts
@@ -33,16 +33,12 @@ export class NativeSDK implements INativeSDK {
     targetNativeSDK: SYNativeSDK | IOSNativeSDK | AndroidNativeSDK;
 
     init(): void {
-        cc.log('[pf][NativeSDK] targetNativeSDK1: ', this.targetNativeSDK);
         if (this.targetNativeSDK) return;
-        cc.log('[pf][NativeSDK] targetNativeSDK2: ', this.targetNativeSDK);
 
         if (!this._system.isNative) {
             this.targetNativeSDK = new SYNativeSDK(this);
         } else if (this._system.isNative && this._system.isIOS) {
-            cc.log('[pf][NativeSDK] targetNativeSDK-ios');
             this.targetNativeSDK = new IOSNativeSDK(this);
-            cc.log('[pf][NativeSDK] targetNativeSDK3: ', this.targetNativeSDK);
         } else if (this._system.isNative && this._system.isAndroid) {
             this.targetNativeSDK = new AndroidNativeSDK(this);
         }
