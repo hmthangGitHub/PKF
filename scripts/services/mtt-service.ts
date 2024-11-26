59cd4a38ead38a2d22bceb7ab4fb9acb6bdb0107
hector.hung <hector.hung@a5labs.co>
2024-11-26 14:52:42 +0800

    add notify event

--- scripts/services/mtt-service.ts
+++ scripts/services/mtt-service.ts
@@ -1,14 +1,9 @@
 import { EmittableService } from '../core/core-index';
-import {
-    type ISocket,
-    type INoticeMttAuth,
-    type INoticeGlobalMessage,
-    MsgType,
-    MttNotifyType
-} from '../poker-client/poker-client-index';
+import type { ISocket, INoticeMttAuth, INoticeGlobalMessage } from '../poker-client/poker-client-index';
 
 export interface MttEvents {
     auth: () => void;
+    onGlobalMessage: (notify: INoticeGlobalMessage) => void;
 }
 
 export class MttService extends EmittableService<MttEvents> {
@@ -51,40 +46,6 @@ export class MttService extends EmittableService<MttEvents> {
     }
 
     onGlobalMessage(notify: INoticeGlobalMessage) {
-        if (notify.msg_type === MsgType.mtt_game_notify) {
-            switch (notify.mttNotifyType) {
-                case MttNotifyType.notify_type_start:
-                case MttNotifyType.notify_type_started:
-                case MttNotifyType.notify_type_1min:
-                case MttNotifyType.notify_type_blockrobot_startgame:
-                case MttNotifyType.notify_type_blockrobot_stopgame: {
-                    // cv.roomManager.mtt_id = notify.mtt_id;
-                    // cv.roomManager.mtt_name = notify.mttGameName;
-                    // cv.roomManager.mtt_time = notify.mttRemainTime;
-                    // let _sureCb = () => {
-                    //     cv.MessageCenter.send('hallEnterMTT');
-                    // };
-                    console.error('MTT started!!!!' + notify.mttNotifyType);
-                    return;
-                }
-                case MttNotifyType.notify_type_30min:
-                case MttNotifyType.notify_type_60min:
-                case MttNotifyType.notify_type_180min:
-                // {
-                //     if (this._shouldShowMttGameTopNotice(msg)) {
-                //         const param: Partial<TNParams> = {
-                //             type: TNTypes.MTT_GAME_START,
-                //             title:  msg.mttGameName,
-                //             roomId: msg.mtt_id,
-                //             time_seconds: msg.mttRemainTime,
-                //             mtt_game_start_type: msg.mttNotifyType,
-                //          };
-
-                //         cv.TN.showMsg(param as TNParams);
-                //     }
-                //     return;
-                // }
-            }
-        }
+        this.emit('onGlobalMessage', notify);
     }
 }
